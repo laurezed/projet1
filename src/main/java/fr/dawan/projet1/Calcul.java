7a0e465329b38b960605c97a0e515c08133984bd
laure <l.zedde@gmail.com>
2019-07-16 14:53:23 +0200

    change

---
+++ src/main/java/fr/dawan/projet1/Calcul.java
@@ -0,0 +1,28 @@
+package fr.dawan.projet1;
+
+
+/**
+ * class utilitaire
+ * @author Admin stagiaire
+ *
+ */
+public class Calcul {
+	
+	private Calcul() {
+		
+	}
+
+	//methode qui prend 2 entiers en paramètre et retourne la somme4
+	//visibilité mot-clef typeretour noMethode (parametre)
+
+	/**
+	 * @author laure zedde
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int somme(int a,int b) {
+		return (a+b);
+	}
+
+}
---
+++ src/test/java/fr/dawan/projet1/Calcul2Test.java
@@ -0,0 +1,17 @@
+package fr.dawan.projet1;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Calcul2Test extends TestCase {
+
+	@Test
+	public void testMultiplier() {
+		assertEquals(6, Calcul2.multiplier(3,2) );
+	}
+
+}
